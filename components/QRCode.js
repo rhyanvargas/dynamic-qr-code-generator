575ac23bb0f4a8d7c2da533d7afe897a258062d4
Rhyguy <rhyan@rhyanvargas.com>
2024-07-14 16:33:33 -0700

    initial working prototype.

---
+++ components/QRCode.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from 'react';
+import QRCode from 'qrcode';
+
+
+export default function QRCodeSection() {
+    const [inputText, setInputText] = useState('');
+    const [qrImage, setQrImage] = useState('');
+
+    const generateQR = async () => {
+        try {
+            // Check if inputText starts with "https://www", if not, prepend it.
+            const formattedText = inputText.startsWith('https://www') ? inputText : `https://www${inputText}`;
+            const qr = await QRCode.toDataURL(formattedText);
+            setQrImage(qr);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                className='text-black'
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+            />
+            <button onClick={generateQR}>Generate QR Code</button>
+            {qrImage && <img src={qrImage} alt="Generated QR Code" />}
+        </div>
+    );
+}
